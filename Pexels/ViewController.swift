ed9dc82e232c5f1f14c222856564ba543cbc1ad0
Alibek Allamzharov <alkhero21@MBP-Alibek.lan>
2023-08-28 17:34:11 +0600

    create Pexels project

---
+++ Pexels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pexels
+//
+//  Created by Alibek Allamzharov on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
